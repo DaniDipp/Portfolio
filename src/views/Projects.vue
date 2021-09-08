faafadacba36a449fbf2843066960046cc5d59f0
Daniel <dippigames@gmail.com>
2021-09-08 16:06:50 +0200

    Add survivorship bias disclaimer

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,4 +1,9 @@
 <template>
+	<p class="header">
+		This is a selection of projects I have completed over the last few years. There is a strong survivorship bias here as many
+		more projects never saw the light of day, due to changing requirements and priorities.<br />
+		If I ever get the chance to get one of them to a presentable state, I will add it here.
+	</p>
 	<div class="body">
 		<Card v-for="project in projects.filter((p) => p.id)" :key="project.id" :project="project" />
 	</div>
@@ -30,6 +35,10 @@
 		display: grid;
 		grid-template-columns: repeat(auto-fit, minmax(450px, 1fr));
 		grid-gap: 1rem;
+		padding: clamp(0px, 0.1vw, 5px);
+	}
+	.header {
+		font-size: clamp(1.1rem, 1.1vw, 1.5rem);
 		padding: clamp(0px, 0.1vw, 5px);
 	}
 </style>
